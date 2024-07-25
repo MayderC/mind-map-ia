b9313f058e30e6ae891de1ee6af8eb2c97b51694
MayderC <maydercortes1@gmail.com>
2024-07-24 22:21:22 -0700

    chore: Refactor summaries page layout and styling

--- src/app/(views)/app/summaries/page.tsx
+++ src/app/(views)/app/summaries/page.tsx
@@ -2,11 +2,11 @@ import { SummaryList } from "@/components/summaries/output/SummaryList";
 
 export default function Summaries(){
   return (
-      <main className="min-[calc(100vh-64px)] h-[calc(100vh-64px)] w-full lg:p-10 p-5 flex flex-wrap">
-        <div className="w-4/12 h-full">
-          <SummaryList summaries={[1, 2, 3, 4, 5]} />
+      <main className="h-[calc(100vh-64px)] w-full lg:p-10 p-5 flex flex-wrap">
+        <div className="w-1/4 h-full">
+          <SummaryList summaries={[1, 2, 3, 4, 5,6,7,8,9,1,2,3,4,5]} />
         </div>
-        <div className="w-9/12 h-full">
+        <div className="w-3/4 h-full">
         
         </div>
       </main>
--- src/components/summaries/output/SummaryList.tsx
+++ src/components/summaries/output/SummaryList.tsx
@@ -6,13 +6,18 @@ interface SummaryListProps {
 
 export const SummaryList = ({summaries}: SummaryListProps) => {
   return (
-    <div className="bg-primary-dark h-full flex justify-center p-5 rounded-lg w-full ">
-      <div className="flex flex-col gap-4">
-        {summaries.map((summary, index) => (
-          <div key={index}>
-            <SummaryItemList />
+    <div className="h-full flex p-5 rounded-lg w-full relative">
+      <div className="bg-primary-dark p-5 rounded-lg flex flex-col gap-4  relative">
+        <div className="overflow-y-scroll flex flex-col gap-4">
+          {summaries.map((summary, index) => (
+            <div key={index}>
+              <SummaryItemList />
+            </div>
+          ))}
           </div>
-        ))}
+          <div className="flex absolute bg-primary-dark w-full left-0 justify-center rounded-lg items-center py-5 bottom-0">
+            <button className="bg-primary-light text-zinc-950 font-semibold p-2 rounded-lg">Add Summary</button>
+        </div>
       </div>
     </div>
 
