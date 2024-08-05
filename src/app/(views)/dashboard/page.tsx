d2af77e2f4bdee27f7c48e148007fbd60727cb8d
Mayder C <maydercortes1@gmail.com>
2024-08-04 17:36:07 -0700

    refactor: Update dashboard page to use Suspense for lazy loading ExcalidrawWrapper component

--- src/app/(views)/dashboard/page.tsx
+++ src/app/(views)/dashboard/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 import dynamic from "next/dynamic";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 const ExcalidrawWrapper = dynamic(
   async () => (await import("@/presentation-ui/components/ExcalidrawWraper/Excalidrawrapper")).default,
@@ -9,7 +10,7 @@ const ExcalidrawWrapper = dynamic(
     loading: () => <p>Loading...</p>,
   },
 );
-export default function Tool(){
+function Tool(){
 
   const searchParams = useSearchParams()
 
@@ -21,5 +22,13 @@ export default function Tool(){
       <main className="min-[calc(100vh-64px)] h-[calc(100vh-64px)] w-full">
         <ExcalidrawWrapper />
       </main>
+  );
+}
+
+export default function Page() {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <Tool />
+    </Suspense>
   );
 }
