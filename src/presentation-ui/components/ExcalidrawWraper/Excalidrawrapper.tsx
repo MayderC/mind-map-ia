5107ae8306000da8bbb20354c6199f4b60e73644
Mayder C <maydercortes1@gmail.com>
2024-08-13 23:00:40 -0700

    feat: init excalidraw api

--- src/presentation-ui/components/ExcalidrawWraper/Excalidrawrapper.tsx
+++ src/presentation-ui/components/ExcalidrawWraper/Excalidrawrapper.tsx
@@ -6,17 +6,65 @@ import { Excalidraw, MainMenu, WelcomeScreen } from "@excalidraw/excalidraw";
 // import { graphToExcalidraw } from "@excalidraw/mermaid-to-excalidraw/dist/graphToExcalidraw";
 // import { useState, useEffect } from 'react'
 import { ExcalidrawElement } from "@excalidraw/excalidraw/types/element/types";
-import { AppState } from "@excalidraw/excalidraw/types/types";
+import { AppState, ExcalidrawImperativeAPI } from "@excalidraw/excalidraw/types/types";
+import { useEffect, useState } from "react";
 
 //import { ButtonStar } from "../Button/ButtonStar/ButtonStar";
 type ExcaliDrawWrapperState = { elements?: ExcalidrawElement[], appState?: AppState }
 const ExcalidrawWrapper: React.FC = () => {
 
+  const [excalidrawAPI, setExcalidrawAPI] = useState<ExcalidrawImperativeAPI | null>(null);
+
+
+  const handleSave = async() => {    
+
+  };
+  
+  const updateScene = () => {
+    if(!excalidrawAPI)return
+    excalidrawAPI.updateScene({
+      elements: [
+        {
+          type: "rectangle",
+          version: 141,
+          versionNonce: 361174001,
+          isDeleted: false,
+          id: "oDVXG8D6Dpl",
+          fillStyle: "hachure",
+          strokeWidth: 1,
+          strokeStyle: "solid",
+          roughness: 1,
+          opacity: 100,
+          angle: 0,
+          x: 200.75,
+          y: 100.5,
+          strokeColor: "#000000",
+          backgroundColor: "transparent",
+          width: 200,
+          height: 100,
+          seed: 1968410350,
+          groupIds: [],
+          roundness: {
+            type: 3,
+            value: 32,
+          },
+          frameId: null, // or the appropriate frameId if applicable
+          boundElements: [], // Array of bound elements
+          updated: Date.now(), // Current timestamp or a specific updated timestamp
+          link: null, // or an appropriate link object if applicable
+          locked: false, 
+        },
+      ],
+    });
+  }
+
 
   return (
     <Excalidraw 
           theme="dark"
-          //excalidrawAPI={(api) => setExcalidrawAPI(api)}
+          excalidrawAPI={(api) => setExcalidrawAPI(api)}
+          UIOptions={{ canvasActions: { saveAsImage: true } }}
+          renderTopRightUI={() => <button onClick={updateScene}>Save</button>}
         >
           <MainMenu>
             <MainMenu.ItemLink href="https://google.com">
